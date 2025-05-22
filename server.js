7026a6696bb8c5a9efc451e413b629c9077155ad
Sunjay Kumar <ak2604745@gmail.com>
2025-05-23 00:23:01 +0500

    home section done

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+app.set('view engine', 'ejs');
+app.use(express.static(path.resolve(__dirname, "public")));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
