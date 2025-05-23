0728c81e2d1c0ac2259156f47cc405752f8ec753
Sunjay Kumar <ak2604745@gmail.com>
2025-05-23 11:51:08 +0500

    updated vercel.json

--- app.js
+++ app.js
@@ -2,13 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 app.set('view engine', 'ejs');
-app.use(express.static(path.resolve(__dirname, "public")));
-
 app.get('/', (req, res) => {
   res.render('index');
 });
-
+app.use(express.static(path.resolve(__dirname, "public")));
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`App is running at PORT:${PORT}`);
 });
