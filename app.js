4f0d6f51d940e5bf55f51880069fa46c9d14bebb
Sunjay Kumar <ak2604745@gmail.com>
2025-05-23 13:37:17 +0500

    dark mode complete for homepage

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
 app.set('view engine', 'ejs');
+// app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.resolve(__dirname, "public")));
 app.get('/', (req, res) => {
   res.render('index');
 });
-app.use(express.static(path.resolve(__dirname, "public")));
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`App is running at PORT:${PORT}`);
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class',
   content: [
     "./views/**/*.ejs",
     "./public/**/*.js",
