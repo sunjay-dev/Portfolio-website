d70faa10adbc15b0637a75ebadb1ac5aba366a6f
Sunjay Kumar <ak2604745@gmail.com>
2025-05-24 18:11:32 +0500

    skill section complete for light theme

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.set('view engine', 'ejs');
 // app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.resolve(__dirname, "public")));
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index.ejs');
 });
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
--- public/js/script.js
+++ public/js/script.js
@@ -3,7 +3,7 @@ let form = document.querySelector('form');
 form.querySelectorAll('input, textarea').forEach(field => {
     field.addEventListener('input', () => {
         field.classList.remove('border-red-500');
-});
+    });
 });
 form.onsubmit = (e) => {
     e.preventDefault();
@@ -36,11 +36,43 @@ form.onsubmit = (e) => {
         })
     }).then(res => { return res.json() })
         .then(data => {
-        email.value = '';
-        name.value = '';
-        message.value ='';
+            email.value = '';
+            name.value = '';
+            message.value = '';
         }).catch(error => {
             console.log(error)
         })
 };
 
+
+
+
+let skills = [
+  { name: "JavaScript", icon: "javascript" },
+  { name: "TypeScript", icon: "typescript-icon" },
+  { name: "Bootstrap", icon: "bootstrap" },
+  { name: "Tailwind CSS", icon: "tailwindcss-icon" },
+  { name: "SASS", icon: "sass" },
+  { name: "Docker", icon: "docker-icon" },
+  { name: "AWS", icon: "aws" },
+  { name: "Git", icon: "git-icon" },
+  { name: "Node.js", icon: "nodejs" },
+  { name: "Express", icon: "express" },
+  { name: "MongoDB", icon: "mongodb-icon" },
+  { name: "MySQL", icon: "mysql" },
+  { name: "Redis", icon: "redis" },
+  { name: "Kafka", icon: "kafka" },
+  { name: "Puppeteer", icon: "puppeteer" },
+  { name: "WordPress", icon: "wordpress-icon" }
+];
+
+let skillsDiv = document.querySelector('#skills');
+skills.forEach(skill => {
+
+    skillsDiv.innerHTML += `
+    <div title="${skill.name}" class="flex space-x-2 sm:items-center cursor-pointer items-end border rounded-md p-4 hover:bg-zinc-100">
+            <iconify-icon icon="logos:${skill.icon}" width="24" height="24"></iconify-icon>
+            <p class="sm:text-xl text-sm text-[1rem] text-[#3d3d3d] font-normal">${skill.name}</p>
+        </div>
+    `
+})
