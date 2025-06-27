5a6a15be27c6f4b8302f5cbf14954457dab1433f
Sunjay Kumar <ak2604745@gmail.com>
2025-06-27 10:37:39 +0500

    auto-fetch projects latest from github profile

--- Frontend/src/components/Projects.tsx
+++ Frontend/src/components/Projects.tsx
@@ -1,10 +1,30 @@
+import { useEffect, useState } from 'react'
 import ProjectCard from './cards/ProjectCard'
-import projects from '../data/projects.json'
+// import projects from '../data/projects.json'
 export default function Projects() {
+
+    const [projects, setProjects] = useState([]);
+    useEffect(() => {
+        const UserName = import.meta.env.VITE_GITHUB_USERNAME;
+        fetch(`https://api.github.com/users/${UserName}/repos?sort=created&direction=desc&per_page=10`)
+            .then(res => {
+                if (!res.ok)
+                    throw new Error('Something went Wrong!')
+                return res.json();
+            })
+            .then(res => {
+                const filtered = res
+                    .filter((r: { fork: boolean }) => !r.fork)
+                    .slice(0, 3);
+                setProjects(filtered);
+            })
+            .catch(err => console.log(err))
+    }, [])
+
     return (
         <section className="min-w-dvw bg-white text-[#3d3d3d] dark:bg-[#1e1e1e] dark:text-white md:px-16 py-16 px-6" id='projects'>
             <h2 className="text-4xl font-bold "><u>Proj</u>ects</h2>
-            {projects.map(project => <ProjectCard key={project.name} project={project}/>)}
+            {projects.map((project, index) => <ProjectCard key={index} project={project} />)}
         </section>
     )
 }
--- Frontend/src/components/cards/ProjectCard.tsx
+++ Frontend/src/components/cards/ProjectCard.tsx
@@ -1,25 +1,54 @@
 import { Icon } from "@iconify/react";
 interface projectInterface {
-  url: string,
-  time: string,
-  imgUrl: string,
+  html_url: string,
+  created_at: string,
+  updated_at: string
+  full_name: string,
   description: string,
-  technology: string[],
-  name: string
+  topics: string[],
+  name: string,
+  homepage: string
 }
 interface Props {
   project: projectInterface
 }
 export default function ProjectCard({ project }: Props) {
+
+  function GenerateImageUrl(repo: string) {
+  return `https://raw.githubusercontent.com/${repo}/main/assets/preview.webp`
+  }
+
+  function formatDate(start: string, end: string): string {
+    const startDate = new Date(start);
+    const endDate = new Date(end);
+
+    const startStr = startDate.toLocaleDateString('en-US', {
+      month: 'short',
+      year: 'numeric',
+    });
+
+    const endStr = endDate.toLocaleDateString('en-US', {
+      month: 'short',
+      year: 'numeric',
+    });
+
+    const isPresent = Math.abs(new Date().getTime() - endDate.getTime()) < 1000 * 60 * 60 * 24 * 7;
+    if (isPresent) return `${startStr} – Present`;
+
+    const sameMonth = startDate.getMonth() === endDate.getMonth() && startDate.getFullYear() === endDate.getFullYear();
+    if (sameMonth) return startStr;
+
+    return `${startStr} – ${endStr}`;
+  }
   return (
-    <div onClick={() => window.open(`${project.url}`, '_blank')} className="cursor-pointer group active:scale-[1.02] mt-14 grid md:grid-cols-3 grid-cols-1 gap-4 md:gap-7 justify-between items-center transition duration-300 transform hover:scale-[1.02] sm:hover:shadow-lg sm:hover:bg-[#f8f8f8] dark:hover:bg-[#2a2a2a] dark:active:bg-[#2a2a2a] sm:hover:p-4 sm:shadow-none shadow-lg sm:bg-transparent sm:p-0 p-4 rounded-xl">
-      <img alt={project.name} src={project.imgUrl} className="md:col-span-1 w-full max-h-44 object-cover select-none sm:p-0 p-2 shadow rounded-xl sm:rounded-md" />
+    <div onClick={() => window.open(`${project.homepage || project.html_url}`, '_blank')} className="cursor-pointer group active:scale-[1.02] mt-14 grid md:grid-cols-3 grid-cols-1 gap-4 md:gap-7 justify-between items-center transition duration-300 transform hover:scale-[1.02] sm:hover:shadow-lg sm:hover:bg-[#f8f8f8] dark:hover:bg-[#2a2a2a] dark:active:bg-[#2a2a2a] sm:hover:p-4 sm:shadow-none shadow-lg sm:bg-transparent sm:p-0 p-4 rounded-xl">
+      <img alt={project.name} src={GenerateImageUrl(project.full_name)} className="md:col-span-1 w-full max-h-44 object-cover select-none sm:p-0 p-2 shadow rounded-xl sm:rounded-md" />
       <div className="md:col-span-2 space-y-2 p-2">
-        <h3 className="text-xs font-medium">{project.time}</h3>
-        <a href={project.url} target="_blank" className="text-lg sm:text-xl font-semibold flex items-center">{project.name}<Icon
+        <h3 className="text-xs font-medium">{formatDate(project.created_at, project.updated_at)}</h3>
+        <a href={project.homepage || project.html_url} target="_blank" className="text-lg sm:text-xl font-semibold flex items-center capitalize">{project.name.replaceAll('-', ' ')}<Icon
           icon="heroicons:arrow-up-right-16-solid" className="ml-1 transition-transform duration-200 group-hover:translate-x-1 group-active:translate-x-1  group-hover:-translate-y-1 group-active:-translate-y-1" width="16" height="16"></Icon></a>
         <p className="font-normal opacity-90 selection:opacity-100">{project.description}</p>
-        {project.technology.map(tech => <button key={tech} className="mr-1 px-3 py-1 rounded-full text-xs leading-5 bg-[#3d3d3d] text-gray-50 dark:text-[#3d3d3d] dark:bg-gray-50">{tech}</button>)}
+        {project.topics.slice(0, 5).map(tech => <button key={tech} className="mr-1 px-3 py-1 rounded-full text-xs leading-5 bg-[#3d3d3d] text-gray-50 dark:text-[#3d3d3d] dark:bg-gray-50 capitalize">{tech.replaceAll('-', ' ')}</button>)}
       </div>
     </div>
   )
